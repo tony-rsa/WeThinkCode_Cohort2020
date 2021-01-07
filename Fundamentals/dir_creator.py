96eb7b8b0f67fec9a9985dd86a926c2e82c8073c
tmuhali <tmuhali@student.wethinkcode.co.za>
2021-01-07 16:00:47 +0200

    new dir

---
+++ Fundamentals/dir_creator.py
@@ -0,0 +1,20 @@
+import os
+
+os.system('wtc-lms topics 505079ba-4393-47ff-a956-330555b09f00 | grep "In Progress" >> topics.txt')
+file = open("topics.txt", "r")
+os.remove("topics.txt")
+topics = file.readlines()
+file.close()
+
+script = list()
+for each in topics:
+    words = each.split(" ")
+    i = 0
+    line = 'mkdir "'
+    while words[i] != "[In":
+        line += words[i] + " "
+        i += 1
+    script.append(line + '"\n')
+
+for each in script:
+  os.system(each)
